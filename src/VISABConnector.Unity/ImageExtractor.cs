657e6a7e36d04bd59111174e8f3d042c4620c62d
VocalTrance <platenpaypal@gmail.com>
2021-07-06 12:36:35 +0200

    Add project for Unity image extraction

---
+++ src/VISABConnector.Unity/ImageExtractor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VISABConnector.Unity
+{
+    public class ImageExtractor
+    {
+        /// <summary>
+        /// TODO: Replace return type with custom type or byte. Replace parameter type with custom type.
+        /// </summary>
+        public IList<object> ExtractImages(IEnumerable<object> objects)
+        {
+            return null;
+        }
+    }
+}
