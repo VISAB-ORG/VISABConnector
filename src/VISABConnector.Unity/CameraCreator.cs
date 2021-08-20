e927a5008672e7699aa915334c5d73c6db86eb79
VocalTrance <platenpaypal@gmail.com>
2021-08-20 15:40:27 +0200

    Remove post build event VISABConnector.Unity, make Camera creator internal

--- src/VISABConnector.Unity/CameraCreator.cs
+++ src/VISABConnector.Unity/CameraCreator.cs
@@ -2,18 +2,18 @@
 
 namespace VISABConnector.Unity
 {
-    public static class CameraCreator
+    internal static class CameraCreator
     {
         /// <summary>
         /// The name of the camera that is created by us, if no camera is given.
         /// </summary>
-        public const string CameraName = "VISABConnector.Unity Camera";
+        internal const string CameraName = "VISABConnector.Unity Camera";
 
         /// <summary>
         /// Creates the camera that will be used for snapshotting the game objects
         /// </summary>
         /// <returns>Returns the created camera</returns>
-        public static Camera CreateCamera()
+        internal static Camera CreateCamera()
         {
             var existing = GameObject.Find(CameraName);
             if (existing && existing.GetComponent<Camera>() != null)
@@ -22,13 +22,8 @@ namespace VISABConnector.Unity
             // Create new gameObject and add camera component
             var gameObject = new GameObject(CameraName);
             var camera = gameObject.AddComponent<Camera>();
-            ConfigureCamera(camera);
 
             return camera;
-        }
-
-        private static void ConfigureCamera(Camera camera)
-        {
         }
     }
 }
